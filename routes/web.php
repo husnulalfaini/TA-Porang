1089098ccfc5e8420b712096cc96cab80b51c3b8
Husnul Alfaini <husnulalfaini3@gmail.com>
2021-03-27 20:38:31 +0700

    membuat halaman monitoring petani

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('ketua_kelompok.konfirmasi_petani');
+    return view('ketua_kelompok.monitoring_petani');
 });
